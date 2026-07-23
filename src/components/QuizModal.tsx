54457423127e536d809d299ffef2cd309b02f769
Camilla Jaeger <caj@cleanwatersupply.dk>
2026-07-23 08:09:47 +0200

    Quiz-billede: beskaering til 42%

--- src/components/QuizModal.tsx
+++ src/components/QuizModal.tsx
@@ -113,7 +113,7 @@ export default function QuizModal() {
           <img
             src="/images/solution-tappested.jpg"
             alt="Baclyser neo-filter monteret på vandhane"
-            className="w-full h-full object-cover object-[center_36%] transition-transform duration-500 group-hover:scale-105"
+            className="w-full h-full object-cover object-[center_42%] transition-transform duration-500 group-hover:scale-105"
           />
           {/* Gradient så teksten nedenunder glider ind */}
           <div className="absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-[#1b32c9] to-transparent" />
