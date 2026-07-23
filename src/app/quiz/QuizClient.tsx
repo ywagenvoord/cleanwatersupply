fdb8e7dfac413ae4b103f3ac3e182c95c83c022c
Camilla Jaeger <caj@cleanwatersupply.dk>
2026-07-23 08:18:17 +0200

    Fix: import useEffect i quiz

--- src/app/quiz/QuizClient.tsx
+++ src/app/quiz/QuizClient.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { Gift, Check, Loader2, Trophy, ChevronRight, ArrowRight } from 'lucide-react'
 import {
